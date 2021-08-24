b275587fa6d466013c9d2040baad6c379dce3394
Carol <dinosaur0514@gmail.com>
2021-08-25 00:43:11 +0800

    Update 2021-08-24T16:43:06.624Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,12 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+var swiper = new Swiper('.multi-lessons-swiper', {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
 //# sourceMappingURL=all.js.map
