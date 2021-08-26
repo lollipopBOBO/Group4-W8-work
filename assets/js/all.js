f0f65cd8dda9f8fc3f06283ca629b8ce3e6610ab
Huchan1124 <garlic1124@gmail.com>
2021-08-26 16:57:04 +0800

    Update 2021-08-26T08:57:00.452Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -10,5 +10,46 @@ var swiper = new Swiper('.multi-lessons-swiper', {
     el: ".swiper-pagination",
     clickable: true
   }
-});
+}); //  聽聽他們怎麼說 swiper
+
+var responseSwiper = document.querySelector('.customer-responses-swiper');
+
+if (responseSwiper) {
+  var _swiper = new Swiper(".customer-responses-swiper", {
+    slidesPerView: 1,
+    grid: {
+      rows: 3,
+      fill: 'row'
+    },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false
+    },
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        grid: {
+          rows: 2,
+          fill: 'row'
+        }
+      },
+      992: {
+        slidesPerView: 3,
+        grid: {
+          rows: 2,
+          fill: 'row'
+        }
+      }
+    }
+  });
+}
 //# sourceMappingURL=all.js.map
