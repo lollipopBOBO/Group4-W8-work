1e40f1897b5d121db49431467e9e182189d00cee
Huchan1124 <garlic1124@gmail.com>
2021-08-26 16:55:16 +0800

    更改swiper-cdn、完成'聽聽他們怎麼說'區塊(含RWD)

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,14 +2,59 @@ $(function() {
   console.log('Hello Bootstrap5');
 });
 
-const swiper = new Swiper('.multi-lessons-swiper', {
-  slidesPerView: "auto",
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+
+ const swiper = new Swiper('.multi-lessons-swiper', {
+   slidesPerView: "auto",
+   spaceBetween: 30,
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
+ }); 
 
 
 
+
+
+//  聽聽他們怎麼說 swiper
+ const responseSwiper = document.querySelector('.customer-responses-swiper');
+ if (responseSwiper) {
+  const swiper = new Swiper(".customer-responses-swiper", {
+    slidesPerView: 1,
+    grid: {
+      rows: 3,
+       fill: 'row',
+    },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+     768: {
+      slidesPerView: 2,
+      grid: {
+        rows: 2,
+         fill: 'row',
+       },
+    
+      },
+     992: {
+     slidesPerView: 3,
+      grid: {
+       rows: 2,
+         fill: 'row',
+       },
+     
+     },
+     }
+  });
+ }
